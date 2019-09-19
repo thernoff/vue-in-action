b8892b65493f254d82de72cba976ff2408146541
thernoff <thernoff@yandex.ru>
2019-09-19 17:03:44 +0300

    Работа с шестой главой: создание однофайлового компонента

---
+++ chapter-6/single-file-component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!--Атрибут scoped ограничивает область видимости CSS текущим компонентом-->
+<style scoped>
+
+</style>
