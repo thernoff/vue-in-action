32a9f5cd8971ae5c8042331282da97e6606968b0
thernoff <thernoff@yandex.ru>
2019-09-20 14:42:28 +0300

    Работа с седьмой главой: добавление нового маршрута для просмотра продукта

--- petstore/src/router/index.js
+++ petstore/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Form from '@/components/Form'
 import Main from '@/components/Main'
+import Product from '@/components/Product'
 
 Vue.use(Router)
 
@@ -10,7 +11,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: iMain,
+      name: 'iMain',
       component: Main,
       props: true // ожидает ли компонент каких-либо входных параметров
     },
@@ -19,6 +20,12 @@ export default new Router({
       name: 'Form',
       component: Form,
       props: true // ожидает ли компонент каких-либо входных параметров
+    },
+    {
+      path: '/product/:id',
+      name: 'Id',
+      component: Product,
+      props: true
     }
   ]
 })
