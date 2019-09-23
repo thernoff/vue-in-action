7340002b2054e5c75b6085487a72dfa8e871514b
thernoff <thernoff@yandex.ru>
2019-09-23 12:21:27 +0300

    Работа с восьмой главой: добавление перехода в проект зоомагазина

--- petstore/src/App.vue
+++ petstore/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -12,5 +14,15 @@ export default {
 
 <style>
 #app {
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
