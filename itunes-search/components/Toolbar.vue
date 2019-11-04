bff4d6f047f58ba2c8ed6a71d96ffa0e81abfb87
thernoff <thernoff@yandex.ru>
2019-11-04 12:56:41 +0300

    Добавлен компонент Toolbar

---
+++ itunes-search/components/Toolbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app color="indigo" dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>
+      <nuxt-link class="title" to="/">iTunes Search</nuxt-link>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn to="/">
+      <v-icon>refresh</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.title {
+  text-decoration: none !important;
+}
+
+.title:visited {
+  color: white;
+}
+</style>
--- itunes-search/layouts/default.vue
+++ itunes-search/layouts/default.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <v-app>
+    <ToolBar />
+    <nuxt />
+  </v-app>
 </template>
+
+<script>
+import ToolBar from "@/components/Toolbar.vue";
+
+export default {
+  components: {
+    ToolBar
+  }
+};
+</script>
 
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,7 +28,9 @@ html {
   box-sizing: border-box;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
